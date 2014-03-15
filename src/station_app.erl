9bd60b90415bb8a97b29a37f016a08b8c7b2fe18
Joshua Weaver <joshuaweaver@gmail.com>
2014-03-14 21:41:40 -0500

    Create initial application
    rebar create-app appid=station

---
+++ src/station_app.erl
@@ -0,0 +1,16 @@
+-module(station_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    station_sup:start_link().
+
+stop(_State) ->
+    ok.
