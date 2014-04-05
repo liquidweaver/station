3ce710cdafd5a59ea4d524c539c8e4d0b0164aaa
Joshua Weaver <joshuaweaver@gmail.com>
2014-04-05 12:04:25 -0500

    Moved private tests to include

--- apps/station/src/s_client.erl
+++ apps/station/src/s_client.erl
@@ -1,13 +1,16 @@
 -module(s_client).
 -behavior(gen_server).
+
+-include("records.hrl").
+
 -export([start_link/0,init/1,terminate/2,handle_info/2]).
 -export([handle_call/3]).
 -export([view_world/0]).
+
 -ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
+-include("private_tests/s_client_tests.hrl").
 -endif.
 
--include("records.hrl").
 
 start_link() ->				 %callback module, init parms, debug parms
 	gen_server:start_link(?MODULE, [], []).
@@ -75,26 +78,3 @@ min_max_coords( [],  MinX, MinY, MaxX, MaxY ) ->
 
 create_tile( X, Y, Contents ) ->
 	#tile{ x = X, y = Y, contents = Contents }.
-
--ifdef(TEST).
-% PRIVATE TESTS
-
-create_tile_creates_a_tile_test() -> 
-	?assertEqual(
-		#tile{ x = xcoord, y = ycoord, contents = somestuff},
-		create_tile( xcoord, ycoord, somestuff ) ).
-
-tiles_to_string_empty_returns_empty_string_test() ->
-	?assertEqual( "", tiles_to_string( [] ) ).
-
-tile_to_string_returns_raster_representation_of_tiles_test() ->
-	Tiles = [ create_tile( 0,0, "X" ), create_tile(1,1,"X"), create_tile(2,2,"B") ],
-	Expected = "X__\n_X_\n__B",
-	?assertEqual( Expected, tiles_to_string( Tiles ) ).
-
-min_max_coords_returns_min_and_max_coords_from_tiles_test() ->
-	Tiles = [ create_tile( 0,0,stuff ), create_tile( 10,22,blah ), create_tile( 4,23,things ) ],
-
-	?assertEqual( {0, 0, 10, 23}, min_max_coords( Tiles ) ).
--endif.
-
