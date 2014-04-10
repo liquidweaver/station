9259d27ca086670d7d6b3db3eb51719d795d5db4
Joshua Weaver <joshuaweaver@gmail.com>
2014-04-10 00:20:22 -0500

    object helper function

--- apps/station/src/client_handler.erl
+++ apps/station/src/client_handler.erl
@@ -6,16 +6,36 @@
 
 request( <<"username">>, Username, State = #client_state{}) when 
   is_binary(Username) andalso byte_size(Username) > 0 ->
-  { {[{tile_data, ?SAMPLE_WORLD_DATA},{world_pos, {[{x, State#client_state.x}, {y, State#client_state.y}]}}]}, State#client_state{ username = Username } };
+  { objects([
+    {tile_data, ?SAMPLE_WORLD_DATA},
+    {world_pos, objects([
+      {x, State#client_state.x},
+      {y, State#client_state.y}
+    ])}
+    ]), State#client_state{ username = Username } };
 
 request( <<"username">>, _, State) ->
-  { {[{error, <<"Invalid username.">>}]}, State };
+  { object(error, <<"Invalid username.">>), State };
 
 request( _, _, State = #client_state{ username = undefined }) ->
-  { {[{need_login, <<"Please pass your username.">>}]}, State};
+  { object(need_login, <<"Please pass your username.">>), State};
 
 request( <<"send_tiles">>, _, State) ->
-  { {[{tile_data, ?SAMPLE_WORLD_DATA},{world_pos, {[{x, State#client_state.x}, {y, State#client_state.y}]}}]}, State };
+  { objects([
+    {tile_data, ?SAMPLE_WORLD_DATA},
+    {world_pos, objects([
+      {x, State#client_state.x},
+      {y, State#client_state.y}
+    ])}
+    ]), State };
 
 request( Unknown, Data, State) ->
   {  {[{unknown_request, {[{ Unknown, Data }]} }]}, State }.
+
+
+objects( Objects ) when is_list(Objects) ->
+  { Objects }.
+
+object( Key, Value ) ->
+  {[{ Key, Value }]}.
+
