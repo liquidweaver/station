bb5fa95d53cb422302abd0d6ea69dccb39b55316
Joshua Weaver <joshuaweaver@gmail.com>
2014-04-18 12:16:18 -0500

    ws/syntax cleanup

--- src/ws_handler.erl
+++ src/ws_handler.erl
@@ -1,20 +1,20 @@
 -module(ws_handler).
 -behaviour(cowboy_websocket_handler).
 -include("records.hrl").
- 
+
 -export([init/3]).
 -export([websocket_init/3]).
 -export([websocket_handle/3]).
 -export([websocket_info/3]).
 -export([websocket_terminate/3]).
- 
+
 init({tcp, http}, _Req, _Opts) ->
   {upgrade, protocol, cowboy_websocket}.
- 
+
 websocket_init(_TransportName, Req, _Opts) ->
   timer:send_after( 0, need_login ),
   {ok, Req, #client_state{} }.
- 
+
 websocket_handle({text, Msg}, Req, State) ->
   {ReplyData, State1} = try jiffy:decode(Msg) of
     {[{ MessageType, Data }]} -> client_handler:request( MessageType, Data, State )
